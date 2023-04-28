b902fb445f3ceec649b7ac19f21ad7d7861fd4e9
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-04-28 14:41:14 -0400

    added carousel scripts here, fixed buttons

--- realestate.js
+++ realestate.js
@@ -156,3 +156,47 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+// carousel scripts
+document.addEventListener("DOMContentLoaded", function () {
+  let currentIndex = 0;
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  const prevButton = document.querySelector('.carousel-control.prev');
+  const nextButton = document.querySelector('.carousel-control.next');
+
+  function showSlide(index) {
+    carouselItems.forEach((item, i) => {
+      if (i === index) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % carouselItems.length;
+    showSlide(currentIndex);
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+    showSlide(currentIndex);
+  }
+
+  // Initialize the carousel by showing the first slide
+  showSlide(currentIndex);
+
+  // Auto transition between slides
+  setInterval(nextSlide, 5000); // Change the value (in milliseconds) to adjust the transition interval
+
+  // Add event listeners for the carousel buttons
+  if (prevButton) {
+    prevButton.addEventListener('click', prevSlide);
+  }
+
+  if (nextButton) {
+    nextButton.addEventListener('click', nextSlide);
+  }
+});
+
