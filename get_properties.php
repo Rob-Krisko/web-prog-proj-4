36bcc172bfeac033b43cc2566a8a86031206d9ba
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-04-29 10:59:29 -0400

    pagination implemented

--- get_properties.php
+++ get_properties.php
@@ -17,8 +17,8 @@ $search = isset($_GET['search']) ? $_GET['search'] : '';
 $homeType = isset($_GET['homeType']) ? $_GET['homeType'] : '';
 $minPrice = isset($_GET['minPrice']) ? intval($_GET['minPrice']) : 0;
 $maxPrice = isset($_GET['maxPrice']) ? intval($_GET['maxPrice']) : PHP_INT_MAX;
-$bedrooms = isset($_GET['bedrooms']) ? $_GET['bedrooms'] : '';
-$bathrooms = isset($_GET['bathrooms']) ? $_GET['bathrooms'] : '';
+$bedrooms = isset($_GET['bedrooms']) ? json_decode($_GET['bedrooms']) : array();
+$bathrooms = isset($_GET['bathrooms']) ? json_decode($_GET['bathrooms']) : array();
 $currentPage = isset($_GET['currentPage']) ? intval($_GET['currentPage']) : 1;
 $itemsPerPage = isset($_GET['itemsPerPage']) ? intval($_GET['itemsPerPage']) : 10;
 
@@ -32,18 +32,31 @@ if ($homeType != '' && $homeType != 'Any') {
     $sql .= "AND property_type = '$homeType' ";
 }
 
-if ($bedrooms != '') {
-    $sql .= "AND bedrooms IN (" . implode(',', array_map('intval', explode(',', $bedrooms))) . ") ";
+if (!empty($bedrooms)) {
+    $sql .= "AND bedrooms IN (" . implode(',', $bedrooms) . ") ";
 }
 
-if ($bathrooms != '') {
-    $sql .= "AND bathrooms IN (" . implode(',', array_map('intval', explode(',', $bathrooms))) . ") ";
+if (!empty($bathrooms)) {
+    $sql .= "AND bathrooms IN (" . implode(',', $bathrooms) . ") ";
 }
+
+// First, fetch the total number of properties that match the filters
+$sqlCount = str_replace("SELECT *", "SELECT COUNT(*) as total", $sql);
+$sqlCount = preg_replace("/LIMIT\\s+\\d+\\s+OFFSET\\s+\\d+$/i", "", $sqlCount);
+$countResult = $conn->query($sqlCount);
+$totalItems = $countResult->fetch_assoc()["total"];
 
 $offset = ($currentPage - 1) * $itemsPerPage;
 $sql .= "LIMIT $itemsPerPage OFFSET $offset";
 
 $result = $conn->query($sql);
+
+if (!$result) {
+    header('Content-Type: application/json', true, 500);
+    echo "Error executing SQL query: " . $conn->error;
+    $conn->close();
+    exit();
+}
 
 $data = array();
 
@@ -53,8 +66,14 @@ if ($result->num_rows > 0) {
     }
 }
 
+// Prepare the response
+$response = array(
+    'properties' => $data,
+    'totalItems' => $totalItems
+);
+
 header('Content-Type: application/json');
-echo json_encode($data, JSON_PRETTY_PRINT);
+echo json_encode($response, JSON_PRETTY_PRINT);
 
 $conn->close();
 ?>
