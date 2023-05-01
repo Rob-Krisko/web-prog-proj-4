cc287735650825ac4dd1befe4c22620f4efb7a52
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-05-01 14:10:17 -0400

    final updates

--- buyer_dashboard.php
+++ buyer_dashboard.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["user_id"]) || $_SESSION["user_type"] != "buyer") {
+    header("Location: index.php");
+    exit;
+}
+?>
+
 <?php include 'header.php'; ?>
 
 <main>
