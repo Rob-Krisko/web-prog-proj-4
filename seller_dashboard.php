ac9744622be3064669951aa5ae2be44e0d93887b
Rob-Krisko <123216722+Rob-Krisko@users.noreply.github.com>
2023-05-01 14:18:50 -0400

    Update seller_dashboard.php

--- seller_dashboard.php
+++ seller_dashboard.php
@@ -29,9 +29,11 @@ $result = $stmt->get_result();
 <?php include("header.php"); ?>
 
     <main>
-        <h2>Seller Dashboard</h2>
+        <div class="sidenav">
+            <h2 id="welcomeMessage">Welcome <?php echo htmlspecialchars($_SESSION['user_name']); ?>!</h2>
+            <p>Manage your properties from here! You can add a new property, or delete your current properties.</p>
+        </div>
         <section class="my-properties-container">
-            <h4>My Properties</h4>
             <?php while ($row = $result->fetch_assoc()) : ?>
                 <div class="property-card" data-property-id="<?php echo $row['id']; ?>">
                     <img src="property_images/<?php echo $row['image'] ? $row['image'] : 'default.jpg'; ?>" alt="Property Image">
@@ -39,14 +41,15 @@ $result = $stmt->get_result();
                         <h5><?php echo $row["address"]; ?></h5>
                         <p>$<?php echo number_format($row["price"]); ?></p>
                     </div>
-                    <button class="delete-property-btn">Remove</button>
+                    <div class="button-container">
+                        <button class="delete-property-btn">Remove</button>
+                        <button class="edit-property-btn" onclick="openEditPropertyModal(<?php echo $row['id']; ?>);">Update</button>
+                    </div>
                 </div>
             <?php endwhile; ?>
 
             <div class="property-card add-property">
-                <a href="new_property.html">
-                    <img src="property_images/add_property_icon.png" alt="Add Property">
-                </a>
+                <img src="property_images/add_property_icon.png" alt="Add Property" id="add-property-icon">
             </div>
         </section>
         <div id="property-modal" class="modal">
@@ -62,6 +65,60 @@ $result = $stmt->get_result();
                 <p id="modal-description"></p>
             </div>
         </div>
+
+        <div id="edit-property-modal" class="modal edit-property-modal">
+            <div class="modal-content">
+                <span class="close" onclick="closeEditPropertyModal();">&times;</span>
+                <h3>Edit Property</h3>
+                <form id="edit-property-form" enctype="multipart/form-data">
+                    <input type="hidden" id="edit-property-id" name="property_id">
+                    <!-- The rest of the form fields go here -->
+                    <div>
+                        <label for="edit-address">Address *</label>
+                        <input type="text" id="edit-address" name="address" placeholder="123 Main St" required>
+                    </div>
+                    <div>
+                        <label for="edit-price">Price *</label>
+                        <input type="number" id="edit-price" name="price" placeholder="0" required>
+                    </div>
+                    <div>
+                        <label for="edit-bedrooms">Bedrooms *</label>
+                        <input type="number" id="edit-bedrooms" name="bedrooms" placeholder="0" required>
+                    </div>
+                    <div>
+                        <label for="edit-bathrooms">Bathrooms *</label>
+                        <input type="number" id="edit-bathrooms" name="bathrooms" placeholder="0" required>
+                    </div>
+                    <div>
+                        <label for="edit-image">Property Image</label>
+                        <input type="file" id="edit-image" name="image" accept="image/*">
+                    </div>
+                    <div>
+                        <label for="edit-type">Property Type</label>
+                        <select id="edit-type" name="type">
+                            <option value="house">House</option>
+                            <option value="apartment">Apartment</option>
+                            <option value="condo">Condo</option>
+                            <option value="townhouse">Townhouse</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="edit-area">Area (sq ft)</label>
+                        <input type="number" id="edit-area" name="area" placeholder="0">
+                    </div>
+                    <div>
+                        <label for="edit-year_built">Year Built</label>
+                        <input type="number" id="edit-year_built" name="year_built" placeholder="2000">
+                    </div>
+                    <div>
+                        <label for="edit-description">Description</label>
+                        <textarea id="edit-description" name="description" rows="4" placeholder="Write a brief description of the property"></textarea>
+                    </div>
+                    <button type="submit">Save Changes</button>
+                </form>
+            </div>
+        </div>
+
 
     </main>
 
@@ -116,7 +173,6 @@ $result = $stmt->get_result();
         </div>
     </div>
 
-
     <script src="dash.js"></script>
     <footer>
         <p>&copy; 2023 Boxed Estates. All rights reserved.</p>
@@ -124,7 +180,6 @@ $result = $stmt->get_result();
 
     </body>
 </html>
-
 
 <?php
 $stmt->close();
